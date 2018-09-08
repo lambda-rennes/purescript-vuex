6bd5ecccd3e6d42e6e6a9515b29bac89a85f5350
Gaël Deest <gael.deest@tweag.io>
2018-09-09 01:19:38 +0200

    Indentation fix

--- web/Store/index.js
+++ web/Store/index.js
@@ -4,23 +4,23 @@ import VueX from "vuex";
 
 const mkStore = Counter => new VueX.Store({
     state: {
-		    count: 1
-	  },
-	  getters: {
-		    stringify: state => ('Stringified state: ' + state.count)
-	  },
+      count: 1
+    },
+    getters: {
+      stringify: state => ('Stringified state: ' + state.count)
+    },
     mutations: {
-		    increment: state => {
-            state.count = Counter.inc(state.count);
-        },
-		    decrement: state => {
-            state.count = Counter.dec(state.count);
-        }
-	  },
+      increment: state => {
+        state.count = Counter.inc(state.count);
+      },
+    decrement: state => {
+        state.count = Counter.dec(state.count);
+      }
+    },
     actions: {
-		    increment: ctx => ctx.commit("increment"),
-		    decrement: ctx => ctx.commit("decrement")
-	  }
+      increment: ctx => ctx.commit("increment"),
+      decrement: ctx => ctx.commit("decrement")
+    }
 });
 
 module.exports = {
